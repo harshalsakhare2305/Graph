df50448225114e87b7d396e5f2adc2e3a07b17bd
harshal Sakhare <118835450+harshalsakhare2305@users.noreply.github.com>
2025-02-09 17:26:43 +0530

    Create Leetcode090225.java

---
+++ Leetcode090225.java
@@ -0,0 +1,16 @@
+//https://leetcode.com/problems/count-number-of-bad-pairs/?envType=daily-question&envId=2025-02-09
+import java.util.HashMap;
+import java.util.Map;
+class Solution {
+    public long countBadPairs(int[] nums) {
+        int n = nums.length;
+        long totalPairs = (long)n * (n - 1) / 2;
+        Map<Integer, Long> diffCount = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            int diff = nums[i] - i;
+            totalPairs -= diffCount.getOrDefault(diff, 0L);
+            diffCount.put(diff, diffCount.getOrDefault(diff, 0L) + 1);
+        }
+        return totalPairs;
+    }
+}
